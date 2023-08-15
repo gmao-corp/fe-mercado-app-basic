f3695340f381b7547cb7ab09436b8a7412c755fa
DgSantanaBR <diiiego.santana2@gmail.com>
2023-08-15 13:11:21 -0300

    estilizacao do menu

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,3 +1,5 @@
+// ======HEADER MENU=======
+
 const menu = document.getElementById('menuLateral');
 const headerMenu = document.createElement('header');
 headerMenu.className = "menu-header";
@@ -8,15 +10,40 @@ imagem.src = './assets/images/Group.svg';
 imagem.className = "logoImg";
 headerMenu.appendChild(imagem);
 
-
 const titleLogo = document.createElement('h1');
 titleLogo.className = "logoCc";
-titleLogo.innerHTML = `CompreCerto`
-headerMenu.appendChild(titleLogo)
+titleLogo.innerHTML = `CompreCerto`;
+headerMenu.appendChild(titleLogo);
 
 const subTitle = document.createElement('h3');
 subTitle.className = "subLogo";
 subTitle.innerHTML = `G.m.a.o`;
 headerMenu.appendChild(subTitle);
 
+// =============================================
 
+// ======NAV MENU======
+
+const navMenu = document.createElement('nav');
+navMenu.className = "navMenu"
+menu.appendChild(navMenu);
+
+const buttonInicio = document.createElement('button');
+buttonInicio.className = "navButtons buttonInico";
+buttonInicio.innerHTML += ` <i class="material-symbols-outlined">home</i>
+<span>Ínicio</span>`;
+navMenu.appendChild(buttonInicio);
+
+const buttonProdutos = document.createElement('button');
+buttonProdutos.className = "navButtons buttonProdutos";
+buttonProdutos.innerHTML += ` <i class="material-symbols-outlined">folder</i>
+<span>Produtos</span>`;
+navMenu.appendChild(buttonProdutos);
+
+const buttonFavoritos = document.createElement('button');
+buttonFavoritos.className = "navButtons buttonFavoritos";
+buttonFavoritos.innerHTML += ` <i class="material-symbols-outlined">star</i>
+<span>Favoritos</span>`;
+navMenu.appendChild(buttonFavoritos);
+
+// ==============================================
